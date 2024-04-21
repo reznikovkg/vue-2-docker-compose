0e58e9775edb4b8cb3878644f691b79a7739b0c4
Konstantin Yakovlev <wayleet@yandex.ru>
2024-04-21 22:23:17 +0700

    feat: add create-recipe page, save card to stor

---
+++ vue/src/components/pages/Recipe.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper">
+    <CardEdit :isEditable="true" @addCard="saveToStor" />
+  </div>
+</template>
+
+<script>
+import CardEdit from "../card/CardComponent.vue";
+
+export default {
+  name: "CreateRecipe",
+  components: {
+    CardEdit,
+  },
+  methods: {
+    saveToStor(data) {
+      this.$store.commit("addOneCard", data);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.wrapper {
+  display: flex;
+  justify-content: center;
+}
+</style>
