0d33bbf986c1900c44d3a5f5cad205a06d6d46f7
Konstantin Yakovlev <wayleet@yandex.ru>
2024-04-21 19:02:28 +0700

    feat: card - add edit ability

--- vue/src/components/card/CardComponent.vue
+++ vue/src/components/card/CardComponent.vue
@@ -1,36 +1,76 @@
 <template>
   <div class="card">
     <div class="img-container">
-      <img :src="link" class="card__img" />
+      <img :src="imgUrl" class="card__img" />
     </div>
-    <div class="info-container">
-      <div class="card__title">{{ title.toUpperCase() }}</div>
-      <div class="card__desc">{{ desc }}</div>
+    <div v-if="isEdit" class="info-container">
+      <input class="card__title-edit" ref="userTitle" />
+      <textarea class="card__desc-edit" ref="userDesc"></textarea>
+      <button @click="closeEditMode">Отмена</button>
+      <button @click="saveEdit">Сохранить изменения</button>
+    </div>
+    <div v-else class="info-container">
+      <div class="card__title">{{ cardTitle.toUpperCase() }}</div>
+      <div class="card__desc">{{ cardDesc }}</div>
+      <button @click="showEditMode" v-if="isEditable">Редактировать</button>
+      <button @click="addCard" v-if="isEditable">Добавить блюдо</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'CardComponent',
-  props: ['imgUrl', 'title', 'desc'],
-  computed: {
-    link() {
-      return (
-        this.imgUrl ||
-        'https://pho-hoai-vietnamese.com/img/placeholders/grey_fork_and_knife.png?clckid=8abfa2cc'
-      );
+  name: "CardComponent",
+  props: {
+    imgUrl: {
+      type: String,
+      default:
+        "https://pho-hoai-vietnamese.com/img/placeholders/grey_fork_and_knife.png?clckid=8abfa2cc",
+    },
+    title: { type: String, default: "Название" },
+    desc: { type: String, default: "Описание или рецепт" },
+    isEditable: { type: Boolean, default: true },
+  },
+  data() {
+    return {
+      cardTitle: this.title,
+      cardDesc: this.desc,
+      cardImg: this.imgUrl,
+      isEdit: false,
+    };
+  },
+  methods: {
+    showEditMode() {
+      this.isEdit = true;
+    },
+    closeEditMode() {
+      this.isEdit = false;
+    },
+    saveEdit() {
+      this.cardTitle = this.$refs.userTitle.value;
+      this.cardDesc = this.$refs.userDesc.value;
+      this.isEdit = false;
+    },
+    addCard() {
+      this.$emit("addCard", {
+        title: this.cardTitle,
+        desc: this.cardDesc,
+        imgUrl: this.cardImg,
+      });
     },
   },
 };
 </script>
 
 <style lang="less" scoped>
+* {
+  box-sizing: border-box;
+}
 .card {
   max-width: 300px;
   border-radius: 9px;
   border: 1px solid #eee;
-  font: 600 22px / 26px 'sf pro display';
+  font: 600 22px / 26px "sf pro display";
   &:hover {
     cursor: pointer;
   }
@@ -54,16 +94,35 @@ export default {
     object-position: center;
     transition: 0.4s;
   }
-  &__title {
+  &__title-edit {
+    width: 100%;
+    border-radius: 9px;
+    border: 1px solid #eee;
+    padding: 6px;
+    &:focus {
+      outline: none;
+    }
   }
   &__desc {
     margin: 3px 0 0 0;
     color: rgba(0, 0, 0, 0.4);
-    font: 15px / 18px 'sf pro display';
+    font: 15px / 18px "sf pro display";
     display: -webkit-box;
     -webkit-line-clamp: 3;
     -webkit-box-orient: vertical;
     overflow: hidden;
+    &-edit {
+      width: 100%;
+      display: block;
+      height: 100px;
+      padding: 6px;
+      margin: 6px 0px;
+      border-radius: 9px;
+      border: 1px solid #eee;
+      &:focus {
+        outline: none;
+      }
+    }
   }
 }
 </style>
