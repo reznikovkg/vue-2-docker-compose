b4c7e0ee382e8cee3be0dedd144754188fc1ee94
Konstantin Yakovlev <wayleet@yandex.ru>
2024-05-01 03:27:46 +0300

    fix: mockData - ru -> eng

--- vue/src/components/catalog/mockData.js
+++ vue/src/components/catalog/mockData.js
@@ -1,30 +1,30 @@
 export const data = [
   {
     id: 1,
-    title: 'том ям веджи',
+    title: 'tom yam veggy',
     imgUrl:
       'https://cdn.foodpicasso.com/assets/2024/01/26/fb3a6e53900180561f9b3b6ddd31a6d1---jpg_1100_1e6e0_convert.webp',
-    desc: 'Кисло-острый тайский суп с овощами. Состав: овощной бульон Том Ям, тайские травы, томаты черри, шампиньоны, брокколи, болгарский перец, кокосовое молоко, корень имбиря, лист лайма, красный лук, кинза, пряный рис.  Хранится при t 18-24 С° 3 часа.',
+    desc: 'Sour spicy Thai soup with vegetables. Ingredients: vegetable broth Tom Yam, Thai herbs, cherry tomatoes, mushrooms, broccoli, bell peppers, coconut milk, ginger root, lime leaf, red onion, cilantro, spicy rice. Stored at t 18-24 C° for 3 hours.',
   },
   {
     id: 2,
-    title: 'том ям кай',
+    title: 'tom yam kai',
     imgUrl:
       'https://cdn.foodpicasso.com/assets/2024/01/26/f11734a0598e98721149b5a6d64170f2---jpg_786x486_79755_convert.webp',
-    desc: 'Кисло-острый тайский суп с курицей и овощами. Состав: бульон Том Ям, тайские травы, корень имбиря, куриное мясо, томаты черри, шампиньоны, кокосовое молоко, красный лук, кинза, пряный рис.  Хранится при t 18-24 С° 3 часа.',
+    desc: 'Sour spicy Thai soup with chicken and vegetables. Ingredients: Tom Yam broth, Thai herbs, ginger root, chicken meat, cherry tomatoes, mushrooms, coconut milk, red onion, cilantro, spicy rice. Stored at t 18-24 C° for 3 hours.',
   },
   {
     id: 3,
-    title: 'том ям кунг',
+    title: 'tom yam kung',
     imgUrl:
       'https://cdn.foodpicasso.com/assets/2024/01/26/57338fda0290eec115f74e5d8ab06057---jpg_786x486_79755_convert.webp',
-    desc: 'Кисло-острый тайский суп с креветками и овощами. Состав: бульон Том Ям, тайские травы, корень имбиря, тигровые, креветки, томаты черри, шампиньоны, красный лук, кокосовое молоко, кинза.  Хранится при t 18-24 С° 3 часа.',
+    desc: 'Sour spicy Thai soup with shrimp and vegetables. Ingredients: Tom Yam broth, Thai herbs, ginger root, tiger, shrimp, cherry tomatoes, mushrooms, red onions, coconut milk, cilantro.  Stored at t 18-24 C° for 3 hours.',
   },
   {
     id: 4,
-    title: 'эби рамен с креветками',
+    title: 'shrimp ebi ramen',
     imgUrl:
       'https://cdn.foodpicasso.com/assets/2024/01/26/a5a7056b30867744ac4b6d7ef7f0393b---jpg_786x486_79755_convert.webp',
-    desc: 'Суп на курином бульоне с креветками, лапшой и яйцом. Состав: бульон из курицы, тонкая лапша, маринованное яйцо аджитама, тигровые креветки, зеленый лук, нори, хондаши, комбу, бонито, ростки бамбука, томаты черри.  Хранится при t 18-24 С° 3 часа.',
+    desc: 'Soup on chicken broth with shrimp, noodles and egg. Ingredients: chicken broth, thin noodles, marinated egg ajitama, tiger shrimp, green onions, nori, hondashi, kombu, bonito, bamboo shoots, cherry tomatoes.  Stored at t 18-24 C° for 3 hours.',
   },
 ];
