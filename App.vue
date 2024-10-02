f138a8140ff8507e8946197529381603e2aa0d65
Vladislav Ivanov <vladislav_ivanovm@mail.ru>
2024-10-02 16:10:33 +0300

    Update App.vue

--- App.vue
+++ App.vue
@@ -1,21 +1,42 @@
+<template>
+  <div id="app">
+    <Quiz :questions="questions" />
+  </div>
+</template>
+
 <script>
-import Quiz from './components/Quiz.vue'
+import Quiz from './components/Quiz.vue';
 
 export default {
   name: 'App',
   components: {
-    Quiz
-  }
-}
+    Quiz,
+  },
+  data() {
+    return {
+      questions: [
+        {
+          question: "Какой город является столицей России?",
+            answers: ["Москва", "Калининград", "Воронеж", "Санкт-Петербург"],
+          correctAnswer: 0,
+        },
+        {
+          question: "Сколько будет 2 + 2?",
+            answers: ["3", "4", "5", "6"],
+          correctAnswer: 1,
+        },
+        {
+          question: "Какая команда отвечает за вывод информации на экран?",
+            answers: ["print", "for", "if", "while"],
+          correctAnswer: 0,
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<template>
- <div id="app">
-    <Quiz />
-  </div>
-</template>
-
-<style scoped>
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   text-align: center;
