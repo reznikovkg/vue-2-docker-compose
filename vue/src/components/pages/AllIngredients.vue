befa19ea1373977dba2c906cec1f5f0b73c7bf2b
Daniil <e4z133o1@protonmail.com>
2024-10-04 02:49:50 +0300

    Add template for CreateIngredient page and basic template for AllIngredients
    Will fix fiels types in input in next commit

--- vue/src/components/pages/AllIngredients.vue
+++ vue/src/components/pages/AllIngredients.vue
@@ -5,6 +5,11 @@
       </nav>
       <RouterView/>
       <h2>Страница со всеми ингредиентами</h2>
+      <div v-for="(ingredient) in ingredients" :key="ingredient.id">
+        <p>
+          <span class="Ингредиенты">{{ ingredient }}</span>
+        </p>
+      </div>
     </div>
   </template>
   
@@ -15,8 +20,23 @@
     computed: {
     ROUTES () {
       return ROUTES
+      },
     },
-  }
+    data() {
+      return {
+        ingredients: [],
+      }
+    },
+    mounted() {
+      console.log(localStorage.key(0))
+      if (localStorage.getItem('ingredients')) {
+        try {
+          this.ingredients = JSON.parse(localStorage.getItem('ingredients'));
+        } catch(e) {
+          localStorage.removeItem('ingredients');
+        }
+      }
+    },
   }
   </script>
   
