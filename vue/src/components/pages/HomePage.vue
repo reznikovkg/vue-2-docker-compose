2a1102de122076b0601aa6f6c917db5ce24b3ae8
anaStays <103378067+anaStays@users.noreply.github.com>
2024-10-06 01:05:33 +0300

    Обновление страниц (#32)
    * pages update
    
    * fixed

---
+++ vue/src/components/pages/HomePage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <StartScreen>
+      <div class="home__buttons">
+        <RouterLink to="/menu" class="home__button-link">
+          <CustomButton type="menu">Начать игру</CustomButton>
+        </RouterLink>
+      </div>
+    </StartScreen>
+  </div>
+</template>
+
+<script>
+import StartScreen from "../parts/PageBackground.vue";
+import CustomButton from "../parts/CustomButton.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    StartScreen,
+    CustomButton,
+  },
+};
+</script>
+
+<style lang="less">
+@import "../../assets/styles/variables.less";
+
+.home {
+  background-color: var(--main-background-color);
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+
+  &__buttons {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  &__button-link {
+    text-decoration: none;
+    z-index: 1;
+  }
+}
+</style>
---
+++ vue/src/components/pages/StartMenuPage.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="menu">
+    <StartScreen>
+      <div class="menu__buttons">
+        <RouterLink to="/" class="menu__button-link">
+          <CustomButton type="menu">Продолжить</CustomButton>
+        </RouterLink>
+        <RouterLink to="/levels" class="menu__button-link">
+          <CustomButton type="menu">Выбрать уровень</CustomButton>
+        </RouterLink>
+        <RouterLink to="/" class="menu__button-link">
+          <CustomButton type="menu">Открыть редактор</CustomButton>
+        </RouterLink>
+      </div>
+    </StartScreen>
+  </div>
+</template>
+
+<script>
+// Импортируем компонент StartScreen
+import StartScreen from "../parts/PageBackground.vue";
+import CustomButton from "../parts/CustomButton.vue";
+
+export default {
+  name: "StartMenuView",
+  components: {
+    StartScreen,
+    CustomButton,
+  },
+};
+</script>
+
+<style lang="less">
+@import "../../assets/styles/variables.less";
+
+.menu {
+  background-color: var(--main-background-color);
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__buttons {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  &__button-link {
+    text-decoration: none;
+    z-index: 1;
+  }
+}
+</style>
