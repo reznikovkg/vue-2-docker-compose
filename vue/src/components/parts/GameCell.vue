a3e164b511af4bbf814aa68143eef7fbaa9f2b3f
anaStays <an_vorotilina@mail.ru>
2024-10-19 03:10:05 +0300

    add cells visuals

--- vue/src/components/parts/GameCell.vue
+++ vue/src/components/parts/GameCell.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="game-cell">
-    <div class="game-cell__body" :class="'game-cell__body--' + getCellTypeName"></div>
+    <div class="game-cell__body" :class="'game-cell__body--' + getCellTypeName">
+      <div
+        v-if="getCellTypeName === 'pot' || getCellTypeName === 'flower-pot'"
+        :class="['game-cell__body--' + getCellTypeName + '-overlay']"
+      ></div>
+      <div
+        v-if="getCellTypeName === 'flower-field' || getCellTypeName === 'flower-pot'"
+        class="game-cell__body--flower"
+      >
+        <div class="game-cell__body--flower-overlay"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,30 +31,122 @@ export default {
 </script>
 
 <style scoped lang="less">
+@import "../../assets/styles/variables.less";
+
 .game-cell {
-  margin: 5px;
+  margin: 3px;
   box-sizing: border-box;
-  width: 50px;
-  height: 50px;
+  width: 80px;
+  height: 80px;
+  position: relative;
   &__body {
     width: 100%;
     height: 100%;
     border-radius: 10%;
+    position: relative;
     &--air {
-      background-color: rgba(255, 255, 255, 0);
+      background-color: var(--air-cell-color);
     }
 
-    &--blue {
-      background-color: rgb(0, 0, 255);
+    &--wall {
+      background-color: var(--wall-cell-color);
+      background-image: url(../../assets/images/wall-cell-image.png);
+      background-size: cover;
+      background-position: center;
     }
 
-    &--purple {
-      background-color: rgb(230, 0, 255);
+    &--field {
+      background-color: var(--field-cell-color);
     }
 
-    &--green {
-      background-color: rgb(13, 255, 0);
+    &--shadow {
+      background-color: var(--shadow-cell-color);
+      background-image: url(../../assets/images/shadow-cell-image.png);
+      background-size: cover;
+      background-position: center;
+    }
+
+    &--sun {
+      background-color: var(--sun-cell-color);
+      background-image: url(../../assets/images/sun-cell-image.png);
+      background-size: cover;
+      background-position: center;
+    }
+
+    &--water {
+      background-color: var(--water-cell-color);
+      background-image: url(../../assets/images/water-cell-image.png);
+      background-size: cover;
+      background-position: center;
+    }
+
+    &--pot {
+      background-color: var(--pot-cell-color);
+
+      &-overlay {
+        position: absolute;
+        border-radius: 10%;
+        width: 60px;
+        height: 60px;
+        top: 10px;
+        left: 10px;
+        background-color: var(--pot-inner-cell-color);
+        z-index: 1;
+      }
+    }
+
+    &--flower-field {
+      background-color: var(--field-cell-color);
+    }
+
+    &--flower-pot {
+      background-color: var(--pot-cell-color);
+
+      &-overlay {
+        position: absolute;
+        border-radius: 10%;
+        width: 60px;
+        height: 60px;
+        top: 10px;
+        left: 10px;
+        background-color: var(--pot-inner-cell-color);
+      }
+    }
+
+    &--flower {
+      background-color: var(--flower-cell-color);
+      border-radius: 10%;
+      width: 50px;
+      height: 50px;
+      top: 15px;
+      left: 15px;
+      position: absolute;
+      z-index: 1;
+
+      &-overlay {
+        position: absolute;
+        background-image: url(../../assets/images/flower-cell-image.png);
+        background-size: cover;
+        background-position: center;
+        width: 50px;
+        height: 50px;
+        z-index: 2;
+        animation: rotateAnimation 3s infinite alternate;
+      }
     }
   }
 }
+
+@keyframes rotateAnimation {
+  0% {
+    transform: rotate(0deg);
+  }
+  50% {
+    transform: rotate(-15deg);
+  }
+  100% {
+    transform: rotate(15deg);
+  }
+}
+
 </style>
