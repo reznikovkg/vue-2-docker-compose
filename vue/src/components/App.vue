3a5b2719696ae0d9f7f446d7e9bc9a603722e151
Maks <vrnmaksim@mail.ru>
2024-12-24 01:11:54 +0300

    rotary

--- vue/src/components/App.vue
+++ vue/src/components/App.vue
@@ -1,18 +1,10 @@
 <template>
   <div>
     <RouterView />
-    <ModalContainer />
   </div>
 </template>
 
 <script>
-import ModalContainer from "@/components/parts/ModalContainer";
-
-export default {
-  components: {
-    ModalContainer
-  }
-}
 </script>
 
 <style lang="less">
