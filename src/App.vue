037a4cafed082e600bcbdd3bbf0a75907923cff3
Danon003 <danielvigerin@yandex.ru>
2025-03-11 23:51:24 +0300

    11.03

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <game />
+  </div>
+</template>
+
+<script>
+import Game from "@/components/Game.vue";
+
+export default {
+  components: { Game },
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  padding: 20px;
+}
+</style>
