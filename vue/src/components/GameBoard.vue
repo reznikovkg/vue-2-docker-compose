9f80106a53a7f9968a8d534b27f049ee3aa00af0
svetlanias <riversesies@outlook.com>
2025-05-03 01:03:12 +0300

    управление

--- vue/src/components/GameBoard.vue
+++ vue/src/components/GameBoard.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="game-board" ref="gameContainer">
     <GameStatus :score="score" />
-
     <Player
         :x="player.x"
         :y="player.y"
@@ -9,7 +8,6 @@
         :height="player.height"
         @move="handlePlayerMove"
     />
-
     <Star
         v-for="(star, index) in stars"
         :key="'star-'+index"
@@ -17,19 +15,12 @@
         :y="star.y"
         :size="starSize"
     />
-
     <Asteroid
         v-for="(asteroid, index) in asteroids"
         :key="'asteroid-'+index"
         :x="asteroid.x"
         :y="asteroid.y"
         :size="asteroidSize"
-    />
-
-    <GameControls
-        @move-left="moveLeft"
-        @move-right="moveRight"
-        @stop-move="stopMove"
     />
   </div>
 </template>
@@ -38,7 +29,6 @@
 import PlayerShip from '@/components/parts/PlayerShip.vue'
 import StarObject from '@/components/parts/StarObject.vue'
 import AsteroidObject from '@/components/parts/AsteroidObject.vue'
-import GameControls from '@/components/parts/GameControls.vue'
 import GameStatus from '@/components/parts/GameStatus.vue'
 import modalsMixin from '@/mixins/modals'
 
@@ -49,7 +39,6 @@ export default {
     Player: PlayerShip,
     Star: StarObject,
     Asteroid: AsteroidObject,
-    GameControls,
     GameStatus
   },
   data() {
@@ -80,17 +69,20 @@ export default {
   mounted() {
     this.initGame()
     window.addEventListener('resize', this.handleResize)
+    window.addEventListener('keydown', this.handleKeyDown)
+    window.addEventListener('keyup', this.handleKeyUp)
   },
   beforeDestroy() {
     this.stopGame()
     window.removeEventListener('resize', this.handleResize)
+    window.removeEventListener('keydown', this.handleKeyDown)
+    window.removeEventListener('keyup', this.handleKeyUp)
   },
   methods: {
     initGame() {
       this.handleResize()
       this.resetGame()
     },
-
     handleResize() {
       const container = this.$refs.gameContainer
       if (container) {
@@ -100,7 +92,6 @@ export default {
         this.player.x = (this.gameWidth - this.player.width) / 2
       }
     },
-
     resetGame() {
       this.stopGame()
       this.stars = []
@@ -111,12 +102,10 @@ export default {
       this.asteroidSpawnCounter = 0
       this.startGame()
     },
-
     startGame() {
       this.isGameRunning = true
       this.gameLoop = requestAnimationFrame(this.gameUpdate)
     },
-
     stopGame() {
       this.isGameRunning = false
       if (this.gameLoop) {
@@ -124,30 +113,29 @@ export default {
         this.gameLoop = null
       }
     },
-
     gameUpdate() {
       if (!this.isGameRunning) return
+
+      // Движение игрока
       if (this.player.moving !== 0) {
         this.player.x += this.player.speed * this.player.moving
         this.player.x = Math.max(0, Math.min(this.gameWidth - this.player.width, this.player.x))
       }
+
       this.spawnObjects()
       this.processObjects()
       this.gameLoop = requestAnimationFrame(this.gameUpdate)
     },
-
     spawnObjects() {
       if (++this.starSpawnCounter >= this.starSpawnRate) {
         this.stars.push(this.createStar())
         this.starSpawnCounter = 0
       }
-
       if (++this.asteroidSpawnCounter >= this.asteroidSpawnRate) {
         this.asteroids.push(this.createAsteroid())
         this.asteroidSpawnCounter = 0
       }
     },
-
     createStar() {
       return {
         x: Math.random() * (this.gameWidth - this.starSize),
@@ -157,7 +145,6 @@ export default {
         height: this.starSize
       }
     },
-
     createAsteroid() {
       return {
         x: Math.random() * (this.gameWidth - this.asteroidSize),
@@ -167,11 +154,9 @@ export default {
         height: this.asteroidSize
       }
     },
-
     processObjects() {
       for (let i = this.stars.length - 1; i >= 0; i--) {
         this.stars[i].y += this.stars[i].speed
-
         if (this.checkCollision(this.player, this.stars[i])) {
           this.score += 10
           this.stars.splice(i, 1)
@@ -182,7 +167,6 @@ export default {
 
       for (let i = this.asteroids.length - 1; i >= 0; i--) {
         this.asteroids[i].y += this.asteroids[i].speed
-
         if (this.checkCollision(this.player, this.asteroids[i])) {
           this.stopGame()
           this.openGameOverModal(this.score, this.resetGame)
@@ -192,7 +176,6 @@ export default {
         }
       }
     },
-
     checkCollision(obj1, obj2) {
       return (
           obj1.x < obj2.x + obj2.width &&
@@ -201,27 +184,41 @@ export default {
           obj1.y + obj1.height > obj2.y
       )
     },
-
     handlePlayerMove(direction) {
       this.player.moving = direction
     },
+    handleKeyDown(event) {
+      if (!this.isGameRunning) return
 
-    moveLeft() {
-      this.player.moving = -1
+      switch (event.key) {
+        case 'ArrowLeft':
+        case 'a':
+          this.player.moving = -1
+          break
+        case 'ArrowRight':
+        case 'd':
+          this.player.moving = 1
+          break
+      }
     },
+    handleKeyUp(event) {
+      if (!this.isGameRunning) return
 
-    moveRight() {
-      this.player.moving = 1
-    },
-
-    stopMove() {
-      this.player.moving = 0
+      switch (event.key) {
+        case 'ArrowLeft':
+        case 'a':
+        case 'ArrowRight':
+        case 'd':
+          this.player.moving = 0
+          break
+      }
     }
   }
 }
 </script>
 
 <style scoped lang="less">
+@import '@/less/const.less';
 .game-board {
   position: relative;
   width: 100%;
