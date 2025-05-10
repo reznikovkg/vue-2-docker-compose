b1bd6bc79a87115e3942710c660328a6867dff8b
svetlanias <riversesies@outlook.com>
2025-05-10 03:06:25 +0300

    enemy

---
+++ vue/src/components/parts/BulletObject.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+      class="bullet-object"
+      :style="bulletStyle"
+      :class="{ 'enemy-bullet': isEnemy }"
+  ></div>
+</template>
+
+<script>
+export default {
+  name: 'BulletObject',
+  props: {
+    position: {
+      type: Object,
+      required: true,
+      default: () => ({ x: 0, y: 0 })
+    },
+    size: {
+      type: Number,
+      default: 8
+    },
+    isEnemy: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    bulletStyle() {
+      return {
+        left: `${this.position.x}px`,
+        top: `${this.position.y}px`,
+        width: `${this.size}px`,
+        height: `${this.size}px`
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.bullet-object {
+  position: absolute;
+  background-color: white;
+  border-radius: 50%;
+
+  &.enemy-bullet {
+    background-color: #ffc155;
+  }
+}
+</style>
---
+++ vue/src/components/parts/EnemyShip.vue
@@ -0,0 +1,46 @@
+<template>
+  <div
+      class="enemy-ship"
+      :style="enemyStyle"
+  ></div>
+</template>
+
+<script>
+export default {
+  name: 'EnemyShip',
+  props: {
+    position: {
+      type: Object,
+      required: true,
+      default: () => ({ x: 0, y: 0 })
+    },
+    dimensions: {
+      type: Object,
+      required: true,
+      default: () => ({ width: 40, height: 40 })
+    },
+    health: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    enemyStyle() {
+      return {
+        left: `${this.position.x}px`,
+        top: `${this.position.y}px`,
+        width: `${this.dimensions.width}px`,
+        height: `${this.dimensions.height}px`,
+        backgroundColor: this.health > 1 ? '#781dc6' : '#a467d5'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.enemy-ship {
+  position: absolute;
+  transition: left 0.1s ease-out;
+}
+</style>
