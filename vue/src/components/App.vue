f66dd636eda9ebe6a91e4283e35e01ab6cb17afe
0Valerie0 <goshakafik@gmail.com>
2025-05-31 14:57:02 +0300

    Update App.vue

--- vue/src/components/App.vue
+++ vue/src/components/App.vue
@@ -1,31 +1,32 @@
-  <template>
-    <div id="app">
-      <Header />
-      <router-view :key="$route.fullPath"/>
-    </div>
-  </template>
-  <script>
-  import Header from '@/components/pages/Header.vue'
-  export default {
-    name: 'App',
-    components: {
-      Header
-    }
+<template>
+  <div id="app">
+    <Header />
+    <router-view :key="$route.fullPath"/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/pages/Header.vue'
+export default {
+  name: 'App',
+  components: {
+    Header
   }
-  </script>
-	
- <style lang="less">
-	@import "@/less/const.less";
+}
+</script>
 
-	html, body, #app {
-		height: 100%;
-	}
+<style lang="less">
+@import "@/less/const.less";
 
-	body {
-		background-image: url('/public/assets/gray_black_shadow.jpg');
-		background-size: cover;
-		background-position: center center;
-		background-repeat: no-repeat;
-		margin: 0;
-	}
+html, body, #app {
+  height: 100%;
+}
+
+body {
+  background-image: url('/public/assets/gray_black_shadow.jpg');
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  margin: 0;
+}//
 </style>
