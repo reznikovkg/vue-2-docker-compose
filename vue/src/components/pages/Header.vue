6645a76e76bbaa3ef4217838c0c71474db300d56
0Valerie0 <goshakafik@gmail.com>
2025-05-31 14:06:19 +0300

    Update Header.vue

--- vue/src/components/pages/Header.vue
+++ vue/src/components/pages/Header.vue
@@ -7,57 +7,94 @@
     <div class="header__search">
       <input class="header__search-input" type="text" placeholder="Поиск...">
     </div>
+    <router-link to="/gallery" class="header__gallery-button">
+      Галерея
+    </router-link>
     <button class="header__menu-button">Меню</button>
   </header>
 </template>
 
 <script>
-export default {
-  name: 'Header'
-}
+	export default {
+		name: 'Header'
+	}
 </script>
 
-<style scoped>
-.header {
-  color: white;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 20px;
-  font-size: 40px;
-  font-weight: bold;
-}
-.header__logo {
-  display: flex;
-  align-items: center;
-}
-.header__logo-image {
-  width: 100px;
-  height: auto;
-  margin-right: 10px;
-}
-.header__search {
-  flex-grow: 1;
-  display: flex;
-  justify-content: center;
-}
-.header__search-input {
-  width: 50%;
-  padding: 8px;
-  font-size: 18px;
-  border: 2px solid #ccc;
-  border-radius: 5px;
-}
-.header__menu-button {
-  background: none;
-  color: white;
-  border: 3px solid white;
-  padding: 10px 20px;
-  font-size: 30px;
-  cursor: pointer;
-  border-radius: 5px;
-}
-.header__menu-button:hover {
-  border: 3px solid red;
-}
+<style scoped lang="less">
+	@import "@/less/const.less";
+
+	.header {
+		color: @cWhite;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 20px;
+		font-size: @fsXXXLarge;
+		font-weight: bold;
+		font-family: @ffOne;
+
+		&__logo {
+			display: flex;
+			align-items: center;
+
+			&-image {
+				width: 100px;
+				height: auto;
+				margin-right: 10px;
+			}
+		}
+
+		&__search {
+			flex-grow: 1;
+			display: flex;
+			justify-content: center;
+
+			&-input {
+				width: 50%;
+				padding: 8px;
+				font-size: @fsMedium;
+				border: @sizeBorderDefault solid @cGray;
+				border-radius: 5px;
+				font-family: @ffOne;
+			}
+		}
+
+		&__gallery-button {
+			margin: 0 20px;
+			padding: 10px 20px;
+			background: @cDarkBlue;
+			color: @cWhite;
+			text-decoration: none;
+			border-radius: 5px;
+			font-size: @fsXXLarge;
+			transition: background 0.3s;
+			font-family: @ffOne;
+
+			&:hover {
+				background: darken(@cDarkBlue, 10%);
+			}
+		}
+
+		&__menu-button {
+			background: none;
+			color: @cWhite;
+			border: @sizeBorderThick solid @cWhite;
+			padding: 10px 20px;
+			font-size: @fsXXLarge;
+			cursor: pointer;
+			border-radius: 5px;
+			font-family: @ffOne;
+
+			&:hover {
+				border-color: @cRed;
+			}
+		}
+	}
+
+	.gradient-text {
+		background: linear-gradient(to right, #ff8a00, #e52e71);
+		-webkit-background-clip: text;
+		background-clip: text;
+		color: transparent;
+	}
 </style>
