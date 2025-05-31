694b43359dedb52706c6149b816efd4e9f218c59
0Valerie0 <goshakafik@gmail.com>
2025-05-31 14:12:30 +0300

    Add files via upload

--- vue/src/components/pages/HomePage.vue
+++ vue/src/components/pages/HomePage.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    <Carousel />
-  </div>
+	<div>
+		<Carousel />
+	</div>
 </template>
 <script>
 import Carousel from './Carousel.vue'
 export default {
-  components: {
-    Carousel
-  }
+	components: {
+		Carousel
+	}
 }
 </script>
