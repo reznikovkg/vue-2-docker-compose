94c5efa383d1fefa669e842d0b0ac6243634d5a2
Yuliya Grischenko <yuliya-280504@mail.ru>
2025-06-06 23:27:44 +0300

    add puzzle shapes

--- vue/src/components/pages/Home.vue
+++ vue/src/components/pages/Home.vue
@@ -84,12 +84,12 @@
 
 <script>
 import PageLayout from '../parts/PageLayout'
-import { ROUTER_NAMES } from "@/router/routes";
-import { mapGetters, mapActions, mapMutations } from "vuex";
-import HelpModal from "@/components/modals/HelpModal.vue";
-import PineForest from '@/assets/PineForest.webp';
-import MonaLisa from '@/assets/MonaLisa.webp';
-import PearlEarring from '@/assets/PearlEarring.webp';
+import { ROUTER_NAMES } from "@/router/routes"
+import { mapGetters, mapActions, mapMutations } from "vuex"
+import HelpModal from "@/components/modals/HelpModal.vue"
+import PineForest from '@/assets/PineForest.webp'
+import MonaLisa from '@/assets/MonaLisa.webp'
+import PearlEarring from '@/assets/PearlEarring.webp'
 
 export default {
   name: ROUTER_NAMES.HOME,
@@ -187,27 +187,27 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 40%;
+    width: 450px;
     border: 2px dashed @cBorderOne;
-    padding: 5% 3%;
+    padding: 55px 30px;
     border-radius: 10px;
     background-color: @cBaseOne;
     &__input {
       font-size: 16px;
       color: gray;
-      width: 100%;
+      width: 450px;
       cursor: pointer;
       font-family: @ffThree;
     }
   }
   &__number {
-    width: 40%;
+    width: 450px;
     border: 2px dashed @cBorderOne;
-    padding: 2% 3%;
+    padding: 25px 30px;
     border-radius: 10px;
     background-color: @cBaseOne;
     &__input {
-      width: 100%;
+      width: 450px;
       border: none;
       outline: none;
       text-align: center;
@@ -218,19 +218,19 @@ export default {
     }
   }
   &__preview {
-    width: 46%;
+    width: 515px;
     border-radius: 10px;
   }
   &__level-button {
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 14%;
-    padding: 1% 2%;
+    width: 160px;
+    padding: 15px;
     border: none;
     border-radius: 10px;
-    margin-left: 1%;
-    margin-right: 1%;
+    margin-left: 9px;
+    margin-right: 9px;
     font-size: 16px;
     color: white;
     background-color: #7C7C7C;
@@ -242,8 +242,8 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 46%;
-    padding: 2% 3%;
+    width: 515px;
+    padding: 20px;
     border: none;
     border-radius: 10px;
     font-size: 16px;
